15557587db111cdc9cc0ab1b722c79f2400240d9
axewpokeman23 <axew16092@gmail.com>
2026-09-01 16:50:34 +1200

    Update code.py

--- code.py
+++ code.py
@@ -2,18 +2,28 @@
     task list:
         - allow this to be used with the GUI
         - temperature control : update external value that will go to the GUI to show the temperature
+        
         - ERROR checking : check every way which the code could be broken
             - should NOT be able to spam the buttons
+            
         - start : starts heating according to temperature set by the blue and yellow buttons
             - temperature cannot be changed while the heating is on
+            
         - stop : stops heating, and allows the temperature to be changed
+        
+        - use the temperature decrease and increase function inside the actual buttons
+            - maybe also turn the "if not button" lines into functions? - optional
+            
+        - holding the button down = passive temperature change while button is down
+        
+        - temperate going below zero for some reason when decrease_temp is ran (and also during the emergency stop temperature reset)
 """
 
 from waveshare import PLC
 import time
 import simpleio
 
-# initilise
+# initialise
 print (" ---------------------------- Starting...")
 IO = PLC()
 IO.init_all()
@@ -30,7 +40,8 @@ COUNT = 0
 
 # switches
 E_STOP_SWITCH = False # for emergency stop
-STOPPED_STATE = True # used to be RUN_STATE = False
+STOPPED_STATE = True # used to be RUN_STATE = False. stopped by default
+HEATING = False # heating switch
 
 
 # led colours - GRB
@@ -44,22 +55,26 @@ YELLOW = [255,170,0]
 RAINBOW = [RED, YELLOW, GREEN, BLUE, BLACK]
 
 # Temperature control
-TEMP = 0
+temp = 80 # default temp
+LOWEST_TEMP = 80
 EMERGENCY_TEMP = 180 # temperate functionally CANNOT go above this
 
 # functions
 def change_LED(colour:list):
     IO.RGB_LED.fill(colour)
-
-def boiler_TEMP_RESET():
-    # and also reset temperature (or just go down)
-        while TEMP != 0:
-            # to simulate it going down progressively
-            time.sleep(0.1)
-            TEMP -= 1
-            
-            # to show the temperature going down
-            print(TEMP)
+    
+# temperature control
+def increase_temp():
+    global temp
+    temp += 10
+    time.sleep(0.3)
+    print(temp)
+    
+def decrease_temp():
+    global temp
+    temp -= 10
+    time.sleep(0.3)
+    print(temp)
 
 while True:
     # inputs
@@ -69,25 +84,34 @@ while True:
     UP_BTN = IO.IX3.value
     DOWN_BTN = IO.IX4.value
     
-    if not START_BTN and STOPPED_STATE:
+    
+    # when you press the green button:
+    if not START_BTN:
         time.sleep(0.2)
         print("green")
         change_LED(GREEN)
-
-        # output :
-        #IO.QX0.value = not IO.QX0.value
-        #IO.QX1.value = not IO.QX0.value
-
-        # rainbow LED!!!
-        """
-        while STOP_BTN:
-            for colour in RAINBOW:
-                time.sleep(1)
-                change_LED(colour)
-        """
-        E_STOP_SWITCH = False
-        STOPPED_STATE = False
-        #RUN_STATE = True
+        if STOPPED_STATE:
+            # output :
+            #IO.QX0.value = not IO.QX0.value
+            #IO.QX1.value = not IO.QX0.value
+        
+            # rainbow LED!!!
+            """
+            while STOP_BTN:
+                    for colour in RAINBOW:
+                        time.sleep(1)
+                        change_LED(colour)
+            """
+            print("powered on" if not E_STOP_SWITCH else "emergency stop disabled - powered on")
+            E_STOP_SWITCH = False
+            STOPPED_STATE = False
+        
+        
+        else: # if person presses the button again and "STOPPED_STATE" is False (currently on), then 
+            # assume person wants the heating to start
+            if temp < EMERGENCY_TEMP:
+                print("heating start - temperature control disabled")
+                HEATING = True
         
     # allow the other buttons to be pressed ONLY if ESTOP is FALSE and runstate is active
     if not E_STOP_SWITCH and not STOPPED_STATE:
@@ -98,10 +122,14 @@ while True:
             change_LED(RED)
             """
             time.sleep(0.2)
-            print("red")
+            #print("red")
             
             # just to check temperature
-            print(TEMP)
+            print(temp)
+            
+            # heating is switched off and temperature changing can work
+            HEATING = False
+            print("heating has been switched off - temperature control enabled")
             
             #change_LED(RED)
     
@@ -112,10 +140,17 @@ while True:
             change_LED(BLACK)
             """
             time.sleep(0.2)
-            print("black")
+            #print("black")
             change_LED(BLACK)
-            boiler_TEMP_RESET()
             
+            
+            while temp != LOWEST_TEMP:
+                # to simulate it going down progressively
+                time.sleep(0.1)
+                decrease_temp()
+                # to show the temperature going down in a cool way
+                print(temp)
+                
             
             # if you press emergency stop everything will stop working
             E_STOP_SWITCH = True
@@ -123,11 +158,11 @@ while True:
             #RUN_STATE = False
             
             
-                
+            
             
             
         # as long as the temperature is below 180 and above 0 you can press the blue and yellow buttons
-        if TEMP < EMERGENCY_TEMP and TEMP > 0:
+        if temp < EMERGENCY_TEMP and temp >= LOWEST_TEMP and not HEATING:
             
             if not UP_BTN and DOWN_BTN: # "and DOWNBTN": to stop people from pressing both buttons at the same time
                 """test
@@ -136,9 +171,9 @@ while True:
                 change_LED(BLUE)
                 """
                 time.sleep(0.2)
-                print("blue")
+                #print("blue")
                 change_LED(BLUE)
-                TEMP += 10
+                increase_temp()
                 
         
             if not DOWN_BTN and UP_BTN: # "and UPBTN": to stop people from pressing both buttons at the same time
@@ -148,11 +183,25 @@ while True:
                 change_LED(YELLOW)
                 """
                 time.sleep(0.2)
-                print("yellow")
+                #print("yellow")
                 change_LED(YELLOW)
                 
                 # update temperature
-                TEMP -= 10
+                decrease_temp()
+        elif not DOWN_BTN or not UP_BTN and not HEATING:
             
+            if temp >= EMERGENCY_TEMP:
+                if not UP_BTN:
+                    time.sleep(1)
+                    print(f'{temp}: cannot go above this temperature! Heating has been switched off automatically.')
+                elif not DOWN_BTN:
+                    decrease_temp()
+                
+            elif temp <= LOWEST_TEMP:
+                if not DOWN_BTN:
+                    time.sleep(1)
+                    print(f'{temp}: cannot go below this temperature.')
+                elif not UP_BTN:
+                    increase_temp()
     #time.sleep(0.01)
     IO.RGB_LED.show()
