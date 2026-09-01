c88438a8b3f7305ac1cdbaac961387015f6b79a4
sellws2 <sellws2@student.eit.ac.nz>
2026-09-01 12:20:29 +1200

    updated

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 from waveshare import PLC
 import time
+import simpleio
 
 # initilise
 print ("Starting...")
@@ -12,8 +13,58 @@ IO.init_all()
 STOPPED_STATE = 0
 RUN_STATE = 1
 E_STOP = 2
+
+# default state
+state = STOPPED_STATE
 """
 
+COUNT = 0
+IO.RGB_LED.fill((0,0,80)) #GRB
+
+while True:
+    # inputs
+    START_BTN = IO.IX0.value
+    STOP_BTN = IO.IX1.value
+    E_STOP = IO.IX2.value
+    UP_BTN = IO.IX3.value
+    DOWN_BTN = IO.IX4.value
+    #count
+    COUNT += 1
+    # START_BTN = IO.IX0 / GI1
+    if not START_BTN:
+        IO.QX0.value = not IO.QX0.value
+        IO.QX1.value = not IO.QX0.value
+        IO.RGB_LED.fill((20,20,20)) # GRB
+        # update waveshare.py to support the buzzer
+        # .tone(port,frequency,length,duration)
+        #simpleio.tone(IO.BUZZER,261,0.25)
+    # STOP_BTN = IO.IX1/ GI2
+    if not STOP_BTN:
+        IO.QX0.value = not IO.QX0.value
+        IO.QX1.value = not IO.QX1.value
+        IO.RGB_LED.fill((0,80,80)) #GRB
+        #simpleio.tone(IO.BUZZER,293,0.25)
+    # E_STOP = IO.IX2 / GI3
+    if not E_STOP:
+        IO.RGB_LED.fill((0,250,0)) # GRB
+        #simpleio.tone(IO.BUZZER,329,0.25)
+        IO.LED.value = not IO.LED.value # no LED on the waveshare board - uses RGB    
+    # UP_BTN = IO.IX3 / GI4
+    if not UP_BTN:
+        IO.QX0.value = not IO.QX0.value
+        IO.QX1.value = not IO.QX1.value
+        IO.RGB_LED.fill((0,80,80)) #GRB
+        #simpleio.tone(IO.BUZZER,349,0.25)
+    # DOWN_BTN = IO.IX4 / GI5
+    if not DOWN_BTN:
+        IO.QX0.value = not IO.QX0.value
+        IO.QX1.value = not IO.QX1.value
+        IO.RGB_LED.fill((0,80,80)) #GRB
+        #simpleio.tone(IO.BUZZER,392,0.25)
+    time.sleep(0.01)
+    IO.RGB_LED.show()
+
+"""
 COUNT = 0
 while True:
     COUNT += 1
@@ -27,17 +78,4 @@ while True:
         IO.QX1.value = not IO.QX0.value
 
     time.sleep(0.01)
-
-
-
-
 """
-while True:
-    if not E_STOP and IO.IX0.value:
-        print("RUN")
-        RUN_STATE = True
-        IO.QX0.value = not IO.QX0.value
-
-"""
-
-
