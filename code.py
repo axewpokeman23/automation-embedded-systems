f00b53866024e6c59202707f2cf52b619c232e7c
axewpokeman23 <axew16092@gmail.com>
2026-09-08 16:05:38 +1200

    Update code.py - post-bang-bang (TRYING!!!!) not working
    doesn't work as it should

--- code.py
+++ code.py
@@ -324,49 +324,32 @@ while True:
             # 20C MIN ROOM TEMP SIM, 200C MAX TEMP SIM
             ACTUAL_TEMPERATURE = mapto(value,0.0, 3.3, 20, 200)
             
-            if ACTUAL_TEMPERATURE in range(80, 180):
+            if int(ACTUAL_TEMPERATURE) in range(80, 180):
                 # if temp is in range 80 - 180, allow servo to move (to fix the angle issue)
                 
                 # servo = pressure valve : 0 = closed 180 = fully open
                 servo_angle = mapto(ACTUAL_TEMPERATURE, 80, 180, 0, 180)
                 try:
-                    IO.SERVO.angle = int(angle)
+                    IO.SERVO.angle = int(servo_angle)
                 except:
-                    print(f"error with angle {angle} = must be within 80-180")
+                    print(f"error with angle {servo_angle} = must be within 80-180")
             
             print(f"temp{ACTUAL_TEMPERATURE:0.2f}")
-            
-            """ BANG BANG - 
-            turn off heater when temperature is in a specific range
-            
-            simulating "on start heating" :
-                when boiler is turned on, will "heat" to 80 and then stop,
-            and then wait for user to specify a temperature.
-            
-            disable
-            
-            """
             in_range = range_check(ACTUAL_TEMPERATURE, SETPOINT)
             
-            # checks if the setpoint and actual temperature are "synchronized":
-            if in_range and int(ACTUAL_TEMPERATURE) in range(80, 180):
-                    # if over 180 or boiler is already heated to 80, then heater is switched off.
-                    HEATER.value = False
-                    LED_colour(BLACK)
-                    print("appropriate boiler conditions")
-                # while the temp hasn't reached the "default" yet.... wait for temp to go back to above 80
-            elif int(ACTUAL_TEMPERATURE) not in range(80, 180):
-                if ACTUAL_TEMPERATURE < 80:
-                    print("less than 80")
-                    # heater is switched on to "heat"
-                    HEATER.value = True
-                    LED_colour(GREEN)
-                elif ACTUAL_TEMPERATURE > 180:
-                    # if over 180, heating switched off and temperature waits to reach below 180
-                    HEATER.value = False
-                    LED_colour(BLACK)
+            # checks: temp above setpoint,
+            # if the setpoint and actual temperature are "synchronized":
+            if (ACTUAL_TEMPERATURE >= SETPOINT+2) or (in_range and int(ACTUAL_TEMPERATURE)):
+                #state = STOPPED_STATE
+                HEATER.value = False
+                LED_colour(BLACK)
                 
-            
+                # checks: if temp less than setpoint, and
+                # if temp is below 80... heating is turned on
+            elif ACTUAL_TEMPERATURE <= SETPOINT+2:
+                HEATER.value = True
+                LED_colour(GREEN)
+                    
             print(f"Temperature set at: {SETPOINT}C. Actual Temperature is: {ACTUAL_TEMPERATURE}C.")
             
             
